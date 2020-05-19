b4870c1d2d476ba57b29d6789cce6a14521e0f03
yarmoliq <yarmoliq@gmail.com>
2020-05-19 21:29:49 +0300

    fixed arguments class. now works

--- coursework/Arguments.h
+++ coursework/Arguments.h
@@ -4,6 +4,8 @@
 #define _ARGUMENTS_H_
 
 #include <string>
+#include <iostream>
+#include <stdexcept>
 
 template<int N>
 class Arguments
@@ -11,13 +13,19 @@ class Arguments
 protected:
 	std::string arguments[N];
 public:
-	Arguments(const std::string(&args)[N]);
+	Arguments(const std::string(&args)[N])
+	{
+		for (int i = 0; i < N; ++i)
+			arguments[i] = args[i];
+	}
 
-	std::string operator[](const int i) const;
+	std::string operator[](const int i) const
+	{
+		if (i < 0 or i >= N)
+			throw std::out_of_range("Arguments index is out of range!");
+		return arguments[i];
+	}
 };
 
 
 #endif // !_ARGUMENTS_H_
-
-
-
--- coursework/main.cpp
+++ coursework/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+
 #include "myInput.h"
+#include "Arguments.h"
 
 int main()
 {
